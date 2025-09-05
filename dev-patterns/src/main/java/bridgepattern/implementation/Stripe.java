9c22979b82c0448eb57bcccffc19e827cc802235
Halil Demir <halil.demir@eu.panasonic.com>
2025-09-05 23:38:53 +0200

    implement bridge pattern

---
+++ dev-patterns/src/main/java/bridgepattern/implementation/Stripe.java
@@ -0,0 +1,10 @@
+package bridgepattern.implementation;
+
+import java.math.BigDecimal;
+
+public class Stripe implements Payment{
+    @Override
+    public void pay(final BigDecimal amount) {
+        System.out.println("Paying " + amount + " with Stripe");
+    }
+}
