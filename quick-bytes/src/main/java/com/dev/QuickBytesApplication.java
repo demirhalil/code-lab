638c788db9f8e4162a7cf9cc3f455fe99d6e65ae
Halil Demir <m.halildemir94@gmail.com>
2026-01-20 17:22:39 +0100

    enable app for resilliency

--- quick-bytes/src/main/java/com/dev/QuickBytesApplication.java
+++ quick-bytes/src/main/java/com/dev/QuickBytesApplication.java
@@ -2,8 +2,10 @@ package com.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.resilience.annotation.EnableResilientMethods;
 
 @SpringBootApplication
+@EnableResilientMethods
 public class QuickBytesApplication {
     public static void main(String[] args) {
         SpringApplication.run(QuickBytesApplication.class, args);
